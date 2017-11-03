00bbe65635b95e480ab0ec15e87f9d60cfc3b24b
otenim <snake_istobelieve@keio.jp>
2017-11-03 12:30:34 +0900

    first

--- test.py
+++ test.py
@@ -60,7 +60,7 @@ def main(args):
     pca = PCA(n_components=3)
     pca.fit(input_data)
     compressed_input_data = pca.transform(input_data)
-    print('compressed: %s' % str(compressed_input_data.shape))
+
     # ===============================
     # Make graph
     # ===============================
@@ -73,7 +73,7 @@ def main(args):
         x = compressed_input_data[i*args.samples_per_class : (i+1)*args.samples_per_class, 0]
         y = compressed_input_data[i*args.samples_per_class : (i+1)*args.samples_per_class, 1]
         z = compressed_input_data[i*args.samples_per_class : (i+1)*args.samples_per_class, 2]
-        ax.plot(x, y, z, marker='o', label=classes(i))
+        ax.plot(x, y, z, marker='o', label=classes[i])
     ax.legend(loc='best')
     plt.savefig('test.png')
 
