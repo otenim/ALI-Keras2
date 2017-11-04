6626d9f3098cc3cb34f054a80dcdb353aa2f4e36
otenim <snake_istobelieve@keio.jp>
2017-11-04 17:50:59 +0900

    add ConvMaxout

--- layers.py
+++ layers.py
@@ -1,14 +1,21 @@
 import keras.backend as K
+from keras.engine.topology import Layer
 
-def conv_maxout(x, n_piece=2):
-    """
-    input_shape: (n, h, w, ch)
-    output_shape: (n, h, w, ch/n_piece)
-    
-    'ch' must be divisible by n_piece.
-    """
-    input_shape = x.get_shape().as_list()
-    n, h, w, ch = input_shape
-    x = K.reshape(x, (-1, h, w, ch//n_piece, piece))
-    x = K.max(x, axis=3)
-    return x
+class ConvMaxout(Layer):
+    def __init__(self, n_piece, **kwargs):
+        self.n_piece = n_piece
+        super(ConvMaxout, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(ConvMaxout, self).build(input_shape)
+
+    def call(self, x):
+        input_shape = x.get_shape().as_list()
+        n, h, w, ch = input_shape
+        x = K.reshape(x, (n, h, w, ch//self.n_piece, self.n_piece))
+        x = K.max(x, axis=3)
+        return x
+
+    def compute_output_shape(self, input_shape):
+        n, h, w, ch = input_shape
+        return (n, h, w, ch//self.n_piece)
